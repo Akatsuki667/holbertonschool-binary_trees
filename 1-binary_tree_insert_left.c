5e70978c991bc31df5a461956bc60a4a6565e48d
Akatsuki667 <qextzy@gmail.com>
2024-12-10 11:40:59 +0100

    add file, add README.md, add code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,18 +1,25 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - Insère noeud enfant gauche d'un autre noeud
+ * @parent: Pointeur vers le noeud parent
+ * @value: Valeur à stocker dans le nouveau noeud
+ * Return: Pointeur vers le noeud créé, ou NULL en cas d'échec
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_child; /* création noeud */
+if (parent == NULL) /* vérification paramètre valide */
+return (NULL);
 new_child = malloc(sizeof(binary_tree_t)); /* allocation mémoire noeud */
 if (new_child == NULL) /* vérification allocation */
 return (NULL);
 new_child->n = value; /* assignation champ n avec paramètre */
 new_child->parent = parent; /* assignation champ parent à paramètres */
 new_child->left = parent->left;
-/* assignation noued enfant à noeud gauche parent */
-if (parent->left != NULL)
+/* assignation noeud gauche parent devient neoud gauche new_child */
+if (parent->left != NULL) /* vérification parent à déjà un enfant */
 parent->left->parent = new_child;
-parent->left = new_child;
+/* Si parent a déjà enfant, MAJ champ parent pointer vers new_child */
+parent->left = new_child; /* new_child devient enfant de gauche parent */
 return (new_child);
 }
-
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - Insère un noeud enfant de droite d'un parent
+ * @parent: Pointeur vers le noeud parent
+ * @value: Valeur à stocker dans le nouveau noeud
+ * Return: Pointeur vers le noeud créé, ou NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_child; /* création noeud */
+if (parent == NULL) /* vérification paramètre valide */
+return (NULL);
+new_child = malloc(sizeof(binary_tree_t)); /* allocation mémoire */
+if (new_child == NULL) /* vérification allocation mémoire */
+return (NULL);
+new_child->n = value; /* assignation champ n avec paramètre */
+new_child->parent = parent; /* assignation champ parent à paramètres */
+new_child->right = parent->right;
+/* assignation noeud droit parent devient neoud droit new_child */
+if (parent->right != NULL) /* vérification parent à déjà un enfant */
+parent->right->parent = new_child;
+/* Si parent a déjà enfant, MAJ champ parent pointer vers new_child */
+parent->right = new_child; /* new_child devient enfant de droite parent */
+return (new_child);
+}
