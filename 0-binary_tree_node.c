8e99f1570494d4fb506be66569e2c7a46b6a5b44
Akatsuki667 <qextzy@gmail.com>
2024-12-10 09:46:47 +0100

    add file, add README.md, add code

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - Crée un nœud d'arbre binaire
+ * @parent: Pointeur vers le noeud parent
+ * @value: Valeur à stocker dans le nouveau noeud
+ * Return: Pointeur vers le nouveau noeud, ou NULL en cas d'échec
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new; /* création noeud */
+new = malloc(sizeof(binary_tree_t)); /* allocation mémoire noeud */
+if (new == NULL) /* vérification allocation */
+return (NULL);
+new->n = value; /* assignation champ n avec paramètre */
+new->parent = parent; /* assignation champ parent avec paramètre */
+return (new);
+}
