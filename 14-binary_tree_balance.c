5230f3e2139aa7aef7c6b73e92bcac25a01795f3
Braga <ibrahim.houmaidi@gmail.com>
2024-12-11 15:11:30 +0100

    task 14 no finish

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left_height, right_height;
+
+if (tree == NULL)
+return (0);
+
+left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+return (left_height > right_height ? left_height : right_height);
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int left_height, right_height;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left != NULL)
+left_height = (int)binary_tree_height(tree->left);
+left_height = 0;
+if (tree->right != NULL)
+right_height = (int)binary_tree_height(tree->right);
+right_height = 0;
+return ( left_height - right_height);
+}
